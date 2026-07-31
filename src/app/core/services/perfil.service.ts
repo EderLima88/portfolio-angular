a91421c45c9c0c72cab4ac35497038312168bbe2
EderLima88 <eder.ebl@gmail.com>
2026-07-31 07:54:51 -0300

    fix: recalibrate definitive rxjs delay to 2s on master branch

--- src/app/core/services/perfil.service.ts
+++ src/app/core/services/perfil.service.ts
@@ -1,6 +1,8 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { delay } from 'rxjs/operators';
+
 export interface Perfil {
   nome: string; cargo: string; formacao: string; resumo: string;
   linkLinkedin: string; linkGithub: string; email: string; telefone: string;
@@ -25,16 +27,24 @@ export class PerfilService {
   private http = inject(HttpClient);
   //private apiUrl = '/dados-portfolio.json';
   private apiUrl = 'https://portfolioapi-eder.onrender.com/api/portfolio';
-  dadosPerfil = signal<Perfil | null>(null);
+
+  carregando = signal<boolean>(true);
+  dadosPerfil = signal<any>(null);
   projetos = signal<Projeto[]>([]);
   certificados = signal<Certificado[]>([]);
 
   carregarTudo() {
-    this.http.get<any>(this.apiUrl).subscribe({
+this.carregando.set(true);
+
+    this.http.get<any>(this.apiUrl).pipe(delay(2000)).subscribe({
       next: (resposta) => {
         this.dadosPerfil.set(resposta);
         this.projetos.set(resposta.projetos);
         this.certificados.set(resposta.certificados);
+
+// Só desliga o Spinner após o término dos 2 segundos de atraso
+        this.carregando.set(false);
+
       },
       error: (err) => console.error('Erro ao ler o arquivo dados-portifolio.json:', err)
     });
